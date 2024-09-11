b9ee83ae52069faa6ba91b2af58fd65590932a9c
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-11 11:37:38 -0300

    feat: retira arquivo de dados e cria outras funções para organização de código

--- main.js
+++ main.js
@@ -1,39 +1,22 @@
-const about = [
-    {
-        id: 1,
-        title: 'Samuel Rocha',
-        src: 'perfil.png',
-        alt: 'foto de perfil',
-        description: 'Desenvolvedor Web Full-Stack'
-    },
-    {
-        id: 2,
-        title: 'Profissional',
-        src: 'tecnologia.jpeg',
-        alt: 'foto de códigos',
-        description: 'Formado em Desenvolvimento Web pela <a href="https://www.betrybe.com/">Trybe</a>, possuo projetos back-end e front-end desenvolvidos nas mais diversas tecnologias. Atualmente integro uma equipe que está desenvolvendo uma startup na área da educação, concebendo desde o seu plano de negócios, pesquisa de tecnologias, até o seu desenvolvimento enquanto código'
-    },
-    {
-        id: 3,
-        title: 'Naturalidade',
-        src: 'SalvadorBonfim.jpg',
-        alt: 'igreja do Bonfim, Salvador',
-        description: 'Sou brasileiro, tenho 32 anos, natural da cidade de Salvador-Ba. Salvador é uma cidade tropical, superpovoada, com traços culturais tipicamente africanos, com festas e ritos que permeiam todo o calendário'
-    },
-    {
-        id: 4,
-        title: 'Perfil',
-        src: 'TecnologiaeEu.jpg',
-        alt: 'homem sentado na areia olhando o mar',
-        description: 'Sou Cristão, pacifista, disciplinado, pontual e adaptável. Tenho como hobbies ler, correr, ir à praia e ver futebol.'
-    },
-]
+import about from "./data/data";
 
 function changeData(operation) {
     const description = document.getElementById('description');
     const title2 = document.getElementById('title');
     const image = document.getElementById('image');
-    let id = about.find(({title}) => title == title2.innerText).id;
+    
+    const newId = generateId(operation)
+
+    const newInfo = about[id - 1];
+    
+    description.innerHTML = newInfo.description;
+    title2.innerText = newInfo.title;
+    image.src = newInfo.src;
+    image.alt = newInfo.alt;
+}
+
+function generateId(operation, title) {
+    let id = about.find(({titleAbout}) => titleAbout == title.innerText).id;
     if (operation == 'sum') {
         id += 1
     } else {
@@ -42,16 +25,10 @@ function changeData(operation) {
     if (id == 0) {
         id = 4
     }
-
     if (id > 4) {
         id = 1
     }
-    console.log(id)
-    const newInfo = about[id - 1];
-    description.innerHTML = newInfo.description;
-    title2.innerText = newInfo.title;
-    image.src = newInfo.src;
-    image.alt = newInfo.alt;
+    return id
 }
 
 window.onload = function () {
